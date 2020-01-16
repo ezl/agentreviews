41f06cb33a49adc1d57e9b63d3373c97631b1cca
ezl <ericzliu@gmail.com>
2020-01-16 18:38:21 +0900

    update nuxt, lint

--- pages/privacy.vue
+++ pages/privacy.vue
@@ -1,8 +1,14 @@
 <template>
   <div>
     <h1>Privacy</h1>
-    <p>We will never sell any of your personal information, including your name or email address.</p>
-    <p>We also use the internet -- it drives us nuts that ANYONE does this. It's crazy that we even have to say this!</p>
+    <p>
+      We will never sell any of your personal information, including your name
+      or email address.
+    </p>
+    <p>
+      We also use the internet -- it drives us nuts that ANYONE does this. It's
+      crazy that we even have to say this!
+    </p>
   </div>
 </template>
 
