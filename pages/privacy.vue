5f6b2505fe1c5547ecf408e2d013011644746286
ezl <ericzliu@gmail.com>
2020-01-16 18:09:59 +0900

    privacy

---
+++ pages/privacy.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Privacy</h1>
+    <p>We will never sell any of your personal information, including your name or email address.</p>
+    <p>We also use the internet -- it drives us nuts that ANYONE does this. It's crazy that we even have to say this!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Privacy Policy'
+    }
+  },
+  components: {},
+  data() {
+    return {}
+  },
+  methods: {}
+}
+</script>
+
+<style scoped></style>
